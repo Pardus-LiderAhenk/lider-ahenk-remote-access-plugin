781584a728f7319945b48ce4e66a019a06b9017c
Volkan Şahin <volkan.sahin@agem.com.tr>
2016-05-05 18:06:09 +0300

    lider-remote-access blueprint error was fixed

--- lider-remote-access/src/main/java/tr/org/liderahenk/remote/access/commands/SetupVNCServerCommand.java
+++ lider-remote-access/src/main/java/tr/org/liderahenk/remote/access/commands/SetupVNCServerCommand.java
@@ -2,23 +2,27 @@ package tr.org.liderahenk.remote.access.commands;
 
 import java.util.ArrayList;
 
+import tr.org.liderahenk.lider.core.api.plugin.ICommand;
 import tr.org.liderahenk.lider.core.api.service.ICommandContext;
 import tr.org.liderahenk.lider.core.api.service.ICommandResult;
 import tr.org.liderahenk.lider.core.api.service.ICommandResultFactory;
 import tr.org.liderahenk.lider.core.api.service.enums.CommandResultStatus;
+import tr.org.liderahenk.remote.access.plugininfo.PluginInfoImpl;
 
 /**
  * 
  * @author <a href="mailto:emre.akkaya@agem.com.tr">Emre Akkaya</a>
  *
  */
-public class SetupVNCServerCommand extends BaseCommand {
+public class SetupVNCServerCommand implements ICommand {
 
 	private ICommandResultFactory resultFactory;
+	private PluginInfoImpl pluginInfo;
 
 	@Override
 	public ICommandResult execute(ICommandContext context) {
-		return resultFactory.create(CommandResultStatus.OK, new ArrayList<String>(), this);
+		return resultFactory.create(CommandResultStatus.OK,
+				new ArrayList<String>(), this);
 	}
 
 	@Override
@@ -38,6 +42,20 @@ public class SetupVNCServerCommand extends BaseCommand {
 
 	public void setResultFactory(ICommandResultFactory resultFactory) {
 		this.resultFactory = resultFactory;
+	}
+
+	public void setPluginInfo(PluginInfoImpl pluginInfo) {
+		this.pluginInfo = pluginInfo;
+	}
+
+	@Override
+	public String getPluginName() {
+		return pluginInfo.getPluginName();
+	}
+
+	@Override
+	public String getPluginVersion() {
+		return pluginInfo.getPluginVersion();
 	}
 
 }
