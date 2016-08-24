d175d5619ae559d19d7eca426fa83cb97d0db60c
Volkan Şahin <bm.volkansahin@gmail.com>
2016-08-24 17:51:21 +0300

    import fixing

--- ahenk-remote-access/remote-access/setup-vnc-server.py
+++ ahenk-remote-access/remote-access/setup-vnc-server.py
@@ -7,8 +7,6 @@ from base64 import b64encode
 from os import urandom
 import json
 
-from base.model.enum.ContentType import ContentType
-from base.model.enum.MessageCode import MessageCode
 from base.plugin.abstract_plugin import AbstractPlugin
 
 
@@ -37,10 +35,13 @@ class SetupVnc(AbstractPlugin):
             data['host'] = ip_addresses
 
             self.logger.debug('[SetupVnc] Response data created')
-            self.context.create_response(code=MessageCode.TASK_PROCESSED.value, message='VNC konfigürasyonu başarılı bir şekilde yapıldı!\nUzak makine kullanıcısının izni için lütfen bekleyiniz...', data=json.dumps(data), content_type=ContentType.APPLICATION_JSON.value)
+            self.context.create_response(code=self.get_message_code().TASK_PROCESSED.value,
+                                         message='VNC konfigürasyonu başarılı bir şekilde yapıldı!\nUzak makine kullanıcısının izni için lütfen bekleyiniz...',
+                                         data=json.dumps(data), content_type=self.get_content_type().APPLICATION_JSON.value)
         except Exception as e:
             self.logger.error('A problem occurred while running VNC server. Error Message: {}'.format(str(e)))
-            self.context.create_response(code=MessageCode.TASK_ERROR.value, message='VNC sunucusu çalışırken bir hata oluştu.')
+            self.context.create_response(code=self.get_message_code().TASK_ERROR.value,
+                                         message='VNC sunucusu çalışırken bir hata oluştu.')
 
     def run_vnc_server(self):
 
@@ -53,7 +54,8 @@ class SetupVnc(AbstractPlugin):
         self.logger.debug('[SetupVnc] VNC server was installed')
 
         self.logger.debug('[SetupVnc] Killing running VNC proceses')
-        self.execute("ps aux | grep x11vnc | grep 'port " + self.port + "' | awk '{print $2}' | xargs kill -9", result=False)
+        self.execute("ps aux | grep x11vnc | grep 'port " + self.port + "' | awk '{print $2}' | xargs kill -9",
+                     result=False)
         self.logger.debug('[SetupVnc] Running VNC proceses were killed')
 
         self.logger.debug('[SetupVnc] Getting display and username...')
@@ -75,10 +77,14 @@ class SetupVnc(AbstractPlugin):
         self.execute('su - {0} -c "mkdir -p /tmp/vncahenk{1}"'.format(params[0], params[0]), result=False)
 
         self.logger.debug('[SetupVnc] Creating password as user')
-        self.execute('su - {0} -c "x11vnc -storepasswd {1} /tmp/vncahenk{2}/x11vncpasswd"'.format(params[0], self.password, params[0]), result=False)
+        self.execute(
+            'su - {0} -c "x11vnc -storepasswd {1} /tmp/vncahenk{2}/x11vncpasswd"'.format(params[0], self.password,
+                                                                                         params[0]), result=False)
 
         self.logger.debug('[SetupVnc] Running VNC server as user.')
-        self.execute('su - {0} -c "x11vnc -accept \'popup\' -rfbport {1} -rfbauth /tmp/vncahenk{2}/x11vncpasswd -o /tmp/vncahenk{3}/vnc.log -display :{4}"'.format(params[0], self.port, params[0], params[0], params[1]), result=False)
+        self.execute(
+            'su - {0} -c "x11vnc -accept \'popup\' -rfbport {1} -rfbauth /tmp/vncahenk{2}/x11vncpasswd -o /tmp/vncahenk{3}/vnc.log -display :{4}"'.format(
+                params[0], self.port, params[0], params[0], params[1]), result=False)
 
     def get_username_display(self):
         result_code, p_out, p_err = self.execute("who | awk '{print $1, $5}' | sed 's/(://' | sed 's/)//'", result=True)
